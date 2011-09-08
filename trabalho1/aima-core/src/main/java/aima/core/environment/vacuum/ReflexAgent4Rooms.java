69a49710a717c75cad04497bdea97e02653bb1b3
gabriel.ferreira.barros <gabriel.ferreira.barros@272f2e90-8684-5d7b-128c-7d3c27edd65e>
2011-09-08 20:48:09 +0000

    Adicionada a classe ReflexAgent4Rooms

---
+++ trabalho1/aima-core/src/main/java/aima/core/environment/vacuum/ReflexAgent4Rooms.java
@@ -0,0 +1,46 @@
+package aima.core.environment.vacuum;
+
+import java.util.LinkedHashSet;
+import java.util.Set;
+
+import aima.core.agent.impl.AbstractAgent;
+import aima.core.agent.impl.aprog.SimpleReflexAgentProgram;
+import aima.core.agent.impl.aprog.simplerule.EQUALCondition;
+import aima.core.agent.impl.aprog.simplerule.Rule;
+
+public abstract class ReflexAgent4Rooms extends AbstractAgent {
+	
+	public ReflexAgent4Rooms(Set<Rule> rules) {
+		super(new SimpleReflexAgentProgram(rules));
+	}
+	
+	public static Set<Rule> getMovementRuleSet() {
+		// Note: Using a LinkedHashSet so that the iteration order (i.e. implied
+		// precedence) of rules can be guaranteed.
+		Set<Rule> rules = new LinkedHashSet<Rule>();
+
+//		rules.add(new Rule(new EQUALCondition(VacuumEnvPercept.ATTRIBUTE_STATE,
+//				VacuumEnvironment.LocationState.Dirty),
+//				VacuumEnvironment.ACTION_SUCK));
+		rules.add(new Rule(new EQUALCondition(
+				VacuumEnvPercept.ATTRIBUTE_AGENT_LOCATION,
+				VacuumEnvironment4Rooms.LOCATION_A),
+				VacuumEnvironment4Rooms.ACTION_MOVE_RIGHT));
+		rules.add(new Rule(new EQUALCondition(
+				VacuumEnvPercept.ATTRIBUTE_AGENT_LOCATION,
+				VacuumEnvironment4Rooms.LOCATION_B),
+				VacuumEnvironment4Rooms.ACTION_MOVE_DOWN));
+		rules.add(new Rule(new EQUALCondition(
+				VacuumEnvPercept.ATTRIBUTE_AGENT_LOCATION,
+				VacuumEnvironment4Rooms.LOCATION_C),
+				VacuumEnvironment4Rooms.ACTION_MOVE_UP));
+		rules.add(new Rule(new EQUALCondition(
+				VacuumEnvPercept.ATTRIBUTE_AGENT_LOCATION,
+				VacuumEnvironment4Rooms.LOCATION_D),
+				VacuumEnvironment4Rooms.ACTION_MOVE_LEFT));
+
+		return rules;
+	}
+	
+	public abstract void addActionRules(Set<Rule> rules);
+}
