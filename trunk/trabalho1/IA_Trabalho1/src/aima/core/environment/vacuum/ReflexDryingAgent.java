e8be9a0a5482bc3cefe50f51b15eee90d08fa104
pplupo <pplupo@272f2e90-8684-5d7b-128c-7d3c27edd65e>
2011-09-08 22:04:34 +0000

    Share project "IA_Trabalho1" into "https://ia201102.googlecode.com/svn"

---
+++ trunk/trabalho1/IA_Trabalho1/src/aima/core/environment/vacuum/ReflexDryingAgent.java
@@ -0,0 +1,19 @@
+package aima.core.environment.vacuum;
+
+import java.util.Set;
+
+import aima.core.agent.impl.aprog.simplerule.Rule;
+
+public class ReflexDryingAgent extends ReflexAgent4Rooms {
+
+	public ReflexDryingAgent() {
+		super();
+	}
+
+	@Override
+	public Set<Rule> getActionRuleSet() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
+}
