5b0e220bfddae2bb30db3683e7405863e1ba806e
pplupo <pplupo@272f2e90-8684-5d7b-128c-7d3c27edd65e>
2011-10-30 19:11:49 +0000

    still trying to walk

--- trunk/maze/src/agent/MazeWalker.java
+++ trunk/maze/src/agent/MazeWalker.java
@@ -16,10 +16,11 @@ public class MazeWalker {
 	private int steps;
 	
 	//Internal state
-	private Map<Vertex, State> states;
+	private Map<Vertex<Position>, State> states;
 	
 	// Environment
-	Maze maze;
+	private Maze maze;
+	
 	private LocationSensor sensor = new LocationSensor();	
 	
 	public MazeWalker(Maze maze) {
@@ -30,24 +31,50 @@ public class MazeWalker {
 		int row = locationPercept[0];
 		int column = locationPercept[1];
 		
-		Vertex<Position> spot = getSpot(maze, Position.getEast(row, column));
-		if (sensor.getPercept(spot) == LocationSensor.WallPercept.WALL) {
-			
-			spot = getSpot(maze, Position.getNorth(row, column));
-			if (sensor.getPercept(spot) == LocationSensor.WallPercept.WALL) {
-				
-				spot = getSpot(maze, Position.getSouth(row, column));
-				if (sensor.getPercept(spot) == LocationSensor.WallPercept.WALL) {
-					
-					spot = getSpot(maze, Position.getWest(row, column));
-					if (sensor.getPercept(spot) == LocationSensor.WallPercept.WALL) {
-						
-					}
-				}
+		Vertex<Position> vertex = getNextVertex(row, column, State.NOT_VISITED);
+		if (vertex != null) {
+			//TODO walk on not visited - remember cycles
+		} else {
+			vertex = getNextVertex(row, column, State.VISITED);
+			if (vertex == null) {
+				return steps;
+			} else {
+				//TODO walk on visited - remember explored
 			}
 		}
-		
 		return getSteps();
+	}
+
+	private Vertex<Position> getNextVertex(int row, int column, State state) {
+		Vertex<Position> spot = getSpot(maze, Position.getEast(row, column));
+		if (sensor.getPercept(spot) != LocationSensor.WallPercept.WALL && getState(spot) == state) {
+			return spot;
+		}
+			
+		spot = getSpot(maze, Position.getNorth(row, column));
+		if (sensor.getPercept(spot) != LocationSensor.WallPercept.WALL && getState(spot) == state) {
+			return spot;
+		}
+				
+		spot = getSpot(maze, Position.getSouth(row, column));
+		if (sensor.getPercept(spot) != LocationSensor.WallPercept.WALL && getState(spot) == state) {
+			return spot;
+		}
+					
+		spot = getSpot(maze, Position.getWest(row, column));
+		if (sensor.getPercept(spot) != LocationSensor.WallPercept.WALL && getState(spot) == state) {
+			return spot;
+		}
+		
+		return null;
+	}
+
+	private State getState(Vertex<Position> spot) {
+		if (states.get(spot) == null) {
+			return State.NOT_VISITED;
+		} else {
+			return states.get(spot);
+		}
 	}
 	
 	public Vertex<Position> getSpot(Maze maze, Position position) {
