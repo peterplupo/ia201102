09f9a1dfd23bf8aa350019c79f9c4be4515fd7a6
pplupo <pplupo@272f2e90-8684-5d7b-128c-7d3c27edd65e>
2011-11-10 18:23:02 +0000

    controller

---
+++ trunk/maze/src/controller/MazeController.java
@@ -0,0 +1,21 @@
+package controller;
+
+import maze.GeneticMazeSelector;
+import maze.Maze;
+
+public class MazeController {
+	public static void main(String[] args) {
+		GeneticMazeSelector selector = new GeneticMazeSelector(1000, 50, 0.40f, 0.5f, 0.01f);
+		
+		while (!selector.hasSelectedMaze()) {
+			selector.newGeneration();
+		}
+		
+		Maze maze = selector.getSelectedMaze();
+		System.out.println(maze);
+		if (maze.isValid()) {
+			System.out.println("is valid");
+		}
+		
+	}
+}
