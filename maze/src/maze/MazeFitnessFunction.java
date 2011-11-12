0aff53048ad060294767595c268c762550a7a21f
pplupo <pplupo@272f2e90-8684-5d7b-128c-7d3c27edd65e>
2011-11-12 07:21:01 +0000

    gaussian fitness

--- maze/src/maze/MazeFitnessFunction.java
+++ maze/src/maze/MazeFitnessFunction.java
@@ -1,7 +1,7 @@
 package maze;
 
 import static java.lang.Math.E;
-import static java.lang.Math.abs;
+import static java.lang.Math.sqrt;
 import static java.lang.Math.pow;
 import static java.lang.Math.round;
 import model.FitnessFunction;
@@ -14,7 +14,8 @@ public class MazeFitnessFunction implements FitnessFunction<Maze> {
 		MazeWalker mazeWalker = new MazeWalker(maze);
 		
 		if (mazeWalker.hasFinished()) {
-			return (int)round(100 * pow(E,-abs(1 - (mazeWalker.pathSize()/80)))); //gaussian curve with max on 80 steps * 40
+//			return round(100 * pow(E,-abs(1 - (mazeWalker.pathSize()/80))));
+			return round(173.2*pow(E,-pow(mazeWalker.pathSize()-80,2)/512)/sqrt(3)); //gaussian curve with max on 80 steps ~= 100 points, 70 (or 90) steps ~= 82
 		}
 		
 		return 0;
