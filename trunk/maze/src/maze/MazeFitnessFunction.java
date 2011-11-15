5304a90669ddec763c729ae373f84502ce37d389
gabriel.ferreira.barros <gabriel.ferreira.barros@272f2e90-8684-5d7b-128c-7d3c27edd65e>
2011-11-15 05:33:27 +0000

    alterações em MazeWalker, GeneticMazeSelector (matingPool e crossover resolvidos), Maze (novo merge()), MazeFitnessFunction (nova fitness function),

--- trunk/maze/src/maze/MazeFitnessFunction.java
+++ trunk/maze/src/maze/MazeFitnessFunction.java
@@ -2,11 +2,10 @@ package maze;
 
 import static java.lang.Math.exp;
 import static java.lang.Math.pow;
-import static java.lang.Math.sqrt;
+import model.FitnessFunction;
 
 import org.apache.log4j.Logger;
 
-import model.FitnessFunction;
 import agent.MazeWalker;
 
 public class MazeFitnessFunction implements FitnessFunction<Maze> {
@@ -18,12 +17,12 @@ public class MazeFitnessFunction implements FitnessFunction<Maze> {
 		
 		MazeWalker mazeWalker = MazeWalker.getInstance(maze);
 		mazeWalker.walk();
-		logger.info("MazeWalker: PATH "+ mazeWalker.pathSize());
+		//logger.info("MazeWalker: PATH "+ mazeWalker.pathSize());
 //		if (mazeWalker.hasFinished()) {
 //			// gaussian curve with max on 80 steps ~= 100 points, 70 (or 90) steps ~= 82
 //			return 173.2/sqrt(3)*exp(-pow(mazeWalker.pathSize()-80,2)/512); 
 //		}
 		
-		return 173.2/sqrt(3)*exp(-pow(mazeWalker.pathSize()-80,2)/512);//return 0.0;
+		return 1000*exp(-pow(mazeWalker.pathSize()-80,2) / 8192);
 	}
 }
