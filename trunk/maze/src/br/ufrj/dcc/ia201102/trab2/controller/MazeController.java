c4d5bcdef9716313866a3473c76cfd603c8d97ac
pplupo <pplupo@272f2e90-8684-5d7b-128c-7d3c27edd65e>
2011-11-20 13:43:10 +0000

    improving log, bug fixes

--- trunk/maze/src/br/ufrj/dcc/ia201102/trab2/controller/MazeController.java
+++ trunk/maze/src/br/ufrj/dcc/ia201102/trab2/controller/MazeController.java
@@ -4,6 +4,7 @@ import org.apache.log4j.Logger;
 
 import br.ufrj.dcc.ia201102.trab2.model.maze.GeneticMazeSelector;
 import br.ufrj.dcc.ia201102.trab2.model.maze.Maze;
+import br.ufrj.dcc.ia201102.trab2.model.maze.MazeFitnessFunction;
 
 
 public class MazeController {
@@ -11,25 +12,24 @@ public class MazeController {
 	private static Logger logger = Logger.getLogger(MazeController.class);
 
 	//size of maze population (if very large, chances are that a valid maze will be generated on the original population)
-	private static int populationSize = 20;
+	private static int populationSize = 9;
 	//size of maze side (total area equals square of the given value)
 	private static int mazeSize = 50;
 	//percentage of selected elite (to be carried unchanged to the next generation)
 	private static float eliteRate = 0.2f;
 	//chance of a mutation occur on a gene (maze column)
-	private static float mutationProbability = 0.001f;
+	private static float mutationProbability = 0.005f;
 	
 	public static void main(String[] args) {
 		logger.info("Creating random mazes.");
 		GeneticMazeSelector selector = new GeneticMazeSelector(populationSize, mazeSize, eliteRate, mutationProbability);
 		
-		logger.info("Generate as many generations as needed until a maze compliant with the criteria is selected");
 		while (!selector.hasSelectedMaze()) {
 			selector.newGeneration();
 		}
 		
-		logger.info("Selected maze:");
 		Maze maze = selector.getSelectedMaze();
-		logger.info(maze.toString());
+		new MazeFitnessFunction().eval(maze);
+		logger.info("Selected maze:" + maze.toString());
 	}
 }
--- trunk/maze/src/br/ufrj/dcc/ia201102/trab2/model/maze/MazeFitnessFunction.java
+++ trunk/maze/src/br/ufrj/dcc/ia201102/trab2/model/maze/MazeFitnessFunction.java
@@ -18,12 +18,13 @@ public class MazeFitnessFunction implements FitnessFunction<Maze> {
 		
 		MazeWalker mazeWalker = MazeWalker.getInstance(maze);
 		mazeWalker.walk();
-		logger.info("Walker completed maze in "+ mazeWalker.pathSize() + " steps.");
+		double eval = 1000*exp(-pow(mazeWalker.pathSize()-80,2) / 8192);
+		logger.info("\t\t walker completed maze in "+ mazeWalker.pathSize() + " steps. Fitness: " + eval +".");
 //		if (mazeWalker.hasFinished()) {
 //			// gaussian curve with max on 80 steps ~= 100 points, 70 (or 90) steps ~= 82
 //			return 173.2/sqrt(3)*exp(-pow(mazeWalker.pathSize()-80,2)/512); 
 //		}
 		
-		return 1000*exp(-pow(mazeWalker.pathSize()-80,2) / 8192);
+		return eval;
 	}
 }
